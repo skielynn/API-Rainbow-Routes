0c8f758187313f4e140015095a9b844de7777b05
Skie <skielynn.1105945@gmail.com>
2023-04-17 18:36:37 -0700

    progress

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,7 @@ const app = express()
 // Declare routes that people can visit on the application
 
 // White Page
-app.get('/:color', function (req, res) {
-    let myColor: unknown = req.params.color
-    if (typeof(myColor) === 'string'){
+app.get('/', function (req: Request, res: Response) {
         res.send(`
             <body style="margin: 0;">
                 <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
